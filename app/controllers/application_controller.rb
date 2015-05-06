2d925d7d37c4312d74d2bcd9a2b00412daf0b559
Dave Strus <dave@davestrus.com>
2015-05-06 19:40:23 -0400

    Use a helper method to load categories for navigation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def categories
+    @categories ||= Category.all
+    @categories
+  end
+
+  helper_method :categories
+  hide_action :categories
 end
